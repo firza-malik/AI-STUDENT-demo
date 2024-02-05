6d733992d49c21bedea25096aa7d7fe547387212
Firzamalik <151676991+firza-malik@users.noreply.github.com>
2024-02-05 05:40:45 -0800

    Add files via upload

---
+++ daliy life problem.py
@@ -0,0 +1,23 @@
+day=input("enter a day:")
+if day == "monday":
+    print("today your 2 classes FUNCTIONAL ENGLISH:( 9;00 to 11:00)")
+    print("and next 2 classes APPLIED CALCULUS: (11:30 to 1:30)")
+elif day == "tuesday":
+    print("today your 2 classes COMPUTER FUNDAMENTAL :(9:00 to 11:00)")
+    print("and next 3 classes PROGRAMMING FUNDAMENTAL :(11:30 to 2:30)")
+elif day == " wednesday":
+    print("today your 1 class FUNCTIONAL ENGLISH:(9:00 to 10:00)")
+    print("next 1 class APPLIED CALCULUS:(10:00 to 11:00)")
+    print("and next PROGRAMMING FUNDAMENTAL LAB:(11:30 to 2:30)")
+elif day == "thursday":
+    print("today your 2 classes ISLAMIC STUDIES:(9:00 to 11:00)")
+    print("today your COMPUTER FUNDAMENTAL LAB:(11:30 to 2:30)")
+elif day == "friday":
+    print("per madical:")
+    print("today your 3 classes PRE CALCULUS:(9:00 to 12:00)")
+    print("per engineering:")
+    print("today your classes off")
+else:
+    print("today your classes off")
+       
+          
